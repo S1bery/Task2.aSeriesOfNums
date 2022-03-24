54636f4bd52445c71301f2a618ca07fff901c09c
S1bery <45617892+S1bery@users.noreply.github.com>
2022-03-24 17:25:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
-// 22, 23, 24, 25, 26, 27, 28, 25, 30, 31, 32, 33, 34, 35, 36
+// 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 - сумма равна 435
 int main() {
-    int numbers[] = {23, 30, 35, 22, 24, 31, 25, 34, 36, 26, 25, 32, 33, 27, 28};
+    int numbers[15] = {23, 30, 25, 22, 24, 31, 25, 34, 35, 26, 29, 32, 33, 27, 28};
 
-    int repetNum;
-    for (int i = 0; i < 15; i ++){
-        for (int j = 0; j < 15; j++) {
-            if (numbers[i] == numbers[j] && i != j)
-                repetNum = numbers[j];
-        }
+    int firstNum = 22; // так как мы сами задаем Х, значит мы знаем первое число последовательности
+    int lastNum = firstNum + 14; // узнаем последнее число последовательности исходя из известного Х
+
+    int correctSum= 0;
+    int currentSum = 0;
+
+    for (int i = 0; i < 15; i++) {
+        currentSum += numbers[i];
     }
-    std::cout << repetNum;
+    for (int i = 0; i < 15; i++) {
+        correctSum += firstNum;
+        firstNum++;
+    }
+
+    int dif = correctSum - currentSum; // узнаем разницу
+    int answer = lastNum - dif;
+
+    std::cout << answer;
+
     return 0;
 }
